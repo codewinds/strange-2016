a15bce51e118bf37a79863f5ab1fd9b49d1c406a
Jeff Barczewski <jeff.barczewski@gmail.com>
2016-09-12 16:56:14 -0500

    rxjs exercises

--- subjects/rxjs/src/exercise.js
+++ subjects/rxjs/src/exercise.js
@@ -1,11 +1,10 @@
 import './util/polyfill'; // first import polyfills
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Rx from 'rxjs';
 
 /*
-  Example which fetches a list of items from a REST api
-  and renders it to the screen. Also logs and renders
-  renders the error message if one occurs.
+
  */
 
 
